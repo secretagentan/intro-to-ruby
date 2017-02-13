836245f1a60c3b4ea0843dd49e6fb0b0baf58b41
an nguyen <hello.an.ng@gmail.com>
2017-02-13 12:16:41 -0800

    adds fizzbuzz

--- hello_world.rb
+++ hello_world.rb
@@ -92,4 +92,18 @@ end
   puts "counting...#{n}"
 }
 
+# fizzbuzz
+def fizzbuzz(n)
+  1.upto(n) do |n|
+    if n % 3 === 0 && n % 5 === 0
+      puts "FizzBuzz"
+    elsif n % 5 === 0
+      puts "Buzz"
+    elsif n % 3 === 0
+      puts "Fizz"
+    else
+      puts n
+    end
+  end
+end
 
